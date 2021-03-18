b200d4037c14faf9d070bbdeafeba1fa58ded76e
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-03-18 13:44:11 +0100

    dijkstra & dijkstra_mod pre-post

--- scripts/dijkstra.py
+++ scripts/dijkstra.py
@@ -1,6 +1,20 @@
 def dijkstra(G, a, b):
+    """
+    Return the shortest path between the vertices a and b by running the dijkstra algorithm
+    :param G: is the matrix of edges costs
+    :param a: is the starting node
+    :param b: is the ending node
+    :return: x in N: x > 0, x equiv shortest path between a and b given G
+    """
     return 1
 
 
 def dijkstra_mod(G, a):
+    """
+    return all the shortest paths between a and the others nodes.
+    :param G: is the matrix of edge costs
+    :param a: is the starting node
+    :return:  L: list, L contains the shortest path between a and all others nodes,
+                obviously the shortest path between a node and itself is 0
+    """
     return 1
