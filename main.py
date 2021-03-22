7d33465f84993cf97de5b70b5281af6e801c87f2
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-03-22 21:19:59 +0100

    lil change

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ from scripts.bellmanFord import Bellman_Ford
 from scripts.dijkstra import Dijkstra
 
 if __name__ == "__main__":
-    def print_mtx(mtx):
-        for elem in mtx:
-            print(elem)
+    print('\t**********************\t')
+    print('\t\t\tRunning')
+    print('\t**********************\t\n')
 
     C = csv_to_mtx('res/graph.csv')
+    print('Costs matrix:\n')
     print(C)
 
-    print("Dijkstra")
+    print("Bellman_Ford:\n")
+    print(Bellman_Ford(C))  # print the matrix returned by Bellman_Ford algorithm
+    print('\n\n\n')
+
+    print("Dijkstra:\n")
     print(Dijkstra(C))  # print the matrix returned by Dijkstra algorithm
     print('\n\n\n')
 
-    print("Bellman_Ford:")
-    print_mtx(Bellman_Ford(C))  # print the matrix returned by Bellman_Ford algorithm
-    print('\n\n\n')
-
-    print("Floyd_Warshall:")
+    print("Floyd_Warshall:\n")
     print("WAITING FOR GG")  # print the matrix returned by Floyd_Warshall algorithm
     print('\n\n\n')
