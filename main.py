c2bd8de9f03439031f31862b099c8c2e3f0421f4
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-03-22 21:51:26 +0100

    add some print('\n')

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ if __name__ == "__main__":
     C = csv_to_mtx('res/graph.csv')
     print('Costs matrix:\n')
     print(C)
+    print("\n\n")
 
     print("Bellman_Ford:\n")
     print(Bellman_Ford(C))  # print the matrix returned by Bellman_Ford algorithm
-    print('\n\n\n')
+    print('\n\n')
 
     print("Dijkstra:\n")
     print(Dijkstra(C))  # print the matrix returned by Dijkstra algorithm
-    print('\n\n\n')
+    print('\n\n')
 
     print("Floyd_Warshall:\n")
     print("WAITING FOR GG")  # print the matrix returned by Floyd_Warshall algorithm
