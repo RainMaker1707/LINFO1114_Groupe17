96efc775c3b46641c84b3a8d7a2e337f8da73140
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2021-03-29 15:14:01 +0200

    add test pattern

--- tests/test.py
+++ tests/test.py
@@ -10,3 +10,23 @@ class TestDijkstra(unittest.TestCase):
 
     def test_dijkstra_two_same_path(self):
         return 1
+
+    def test_dijkstra_one_disjoint_point(self):
+        return 1
+
+    def test_dijkstra_disjoint_points(self):
+        return 1
+    
+
+class TestBellmanFord(unittest.TestCase):
+    def test_bellman_ford(self):
+        return 1
+
+    def test_bellman_ford_bad_matrix(self):
+        return 1
+
+    def test_bellman_ford_one_disjoint_point(self):
+        return 1
+
+    def test_bellman_ford_disjoint_points(self):
+        return 1
